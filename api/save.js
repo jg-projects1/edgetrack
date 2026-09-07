80047219ef06c12b4e4a7e96b71efb58509b5c53
jg-projects1 <johngonzales993@gmail.com>
2026-09-07 09:58:49 +0100

    Update save.js

--- api/save.js
+++ api/save.js
@@ -51,6 +51,8 @@ export default async function handler(req, res) {
       'gala': 'Gala Casino',
       'bet st george': 'BetStGeorge',
       'betstgeorge': 'BetStGeorge', // lowercase form only; exact-case dupes handled below
+      'grosvenor casinos': 'Grosvenor',
+      'planet sports': 'Planet Sport Bet',
     };
     const dedupeBookies = (bk) => {
       if (!bk) return bk;
